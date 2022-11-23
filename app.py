bccc7ab434899c3447f82604464b076c3c9b8da1
Jabez Modichai <106453288+Jabez-m@users.noreply.github.com>
2022-11-23 20:46:44 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,44 @@
+import streamlit as st
+import joblib
+vect1=joblib.load('D:/Academics/Personal/Tasks/try4/vect.joblib')
+mod=joblib.load('D:/Academics/Personal/Tasks/try4/cl.ml')
+
+def home(input_data):
+    ls=[]
+    ls.clear()
+    ls.append(input_data)
+    p=vect1.transform(ls)
+    pred=mod.predict(p)
+    
+    if pred==1:
+        return 'Spam'
+    else:
+        return 'Ham'
+    
+    
+def main():
+    st.title('Spam Detection Web App')
+    st.text('This program classifies text as either \'spam\' or \'ham\'')
+    
+    #input
+    text=st.text_input('Input some text in space below:')
+    
+    #prediction
+    clfn=''
+    
+    #button
+    if st.button('Classify Text'):
+        clfn=home(text)
+    st.success(clfn)
+    
+if __name__=='__main__':
+    main()
+          
+    
+    
+    
+    
+    
+
+
+
